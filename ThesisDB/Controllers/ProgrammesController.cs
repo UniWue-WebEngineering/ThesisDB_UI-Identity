a800ecec0439a9bb03c1da9c84fca9e5584b99ff
fredericthiesse <frederic.thiesse@uni-wuerzburg.de>
2026-05-13 17:50:26 +0200

    ProgrammesController

---
+++ ThesisDB/Controllers/ProgrammesController.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using ThesisDB.Models;
+
+namespace ThesisDB.Controllers
+{
+    public class ProgrammesController : Controller
+    {
+        private readonly ThesisDbContext _context;
+
+        public ProgrammesController(ThesisDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Programmes
+        public async Task<IActionResult> Index()
+        {
+            var programmes = await _context.Programmes
+                .Include(p => p.Theses)
+                .OrderBy(p => p.Name)
+                .ToListAsync();
+            return View(programmes);
+        }
+
+        // POST: Programmes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Programme programme)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(programme);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Programmes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Programme programme)
+        {
+            if (id != programme.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(programme);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgrammeExists(programme.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Programmes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var programme = await _context.Programmes.FindAsync(id);
+            _context.Programmes.Remove(programme);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProgrammeExists(int id)
+        {
+            return _context.Programmes.Any(e => e.Id == id);
+        }
+    }
+}
